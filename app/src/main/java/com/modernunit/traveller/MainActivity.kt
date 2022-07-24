deb872ea4ba7ae373232d9114d0489229c0f57c6
Taras Vovchenko <vtaras15@gmail.com>
2022-07-24 06:07:05 +0300

    TR-1: added base app theme

--- app/src/main/java/com/modernunit/traveller/MainActivity.kt
+++ app/src/main/java/com/modernunit/traveller/MainActivity.kt
@@ -3,41 +3,31 @@ package com.modernunit.traveller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.modernunit.traveller.ui.theme.TravellerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             TravellerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Scaffold {
+                    Text(
+                        "Android",
+                        modifier = Modifier
+                            .padding(
+                                WindowInsets.statusBars
+                                    .only(WindowInsetsSides.Horizontal + WindowInsetsSides.Top)
+                                    .asPaddingValues()
+                            ),
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TravellerTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/modernunit/traveller/ui/theme/Color.kt
+++ app/src/main/java/com/modernunit/traveller/ui/theme/Color.kt
@@ -1,8 +1,34 @@
 package com.modernunit.traveller.ui.theme
 
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+private val DarkColorPalette = darkColors(
+    primary = Color(0xFF00CEC9),
+    primaryVariant = Color(0xFF1C1F22),
+    secondary = Color(0xFFF8F8F8),
+    background = Color(0xFF101113),
+    surface = Color(0xFF101113),
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    error = Color(0xFFEA4335)
+)
+
+private val LightColorPalette = lightColors(
+    primary = Color(0xFF00CEC9),
+    primaryVariant = Color(0xFFEAF0F4),
+    secondary = Color(0xFF30353E),
+    background = Color(0xFFF6F8F9),
+    surface = Color(0xFFF6F8F9),
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    error = Color(0xFFEA4335)
+)
+
+fun getThemeColors(isInDarkTheme: Boolean) =
+    if (isInDarkTheme) DarkColorPalette else LightColorPalette
