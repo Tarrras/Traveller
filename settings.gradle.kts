1e23ec1a5bf4bc35593c5258f80f15c348920b30
Taras Vovchenko <vtaras15@gmail.com>
2022-08-11 22:34:31 +0300

    TR-12: added custom gradle plugin for future modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
+pluginManagement {
+    includeBuild("build-core")
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 enableFeaturePreview("VERSION_CATALOGS")
 rootProject.name = "Traveller"
 include(":app")
